a520eba08492f5e0158f4e0f42d5ec7053b8b26d
clarkclark <44596823+clarkclark@users.noreply.github.com>
2018-11-24 19:48:08 -0800

    Update amp-active.ino
    cleaned up presentation, delete archaic lines.

--- amp-active.ino
+++ amp-active.ino
@@ -1,8 +1,7 @@
 /*
-2018-10-29 cewood		amp-active
+2018-11-24	 cewood		amp-active
 context: Arduino Nano clone
-note: BOTH the sound module and the relay are active LOW
-	    this will matter in line 44 digitalRead(detpin);
+note: the relay is active LOW
 */
 
 // declare pins
@@ -12,18 +11,17 @@ int directSense = 0;    // variable to put the sensepin reading into
 
 // declare variables
 int stayOn = 10;	      // seconds (loops) for relay (and amp) to stay on after sound is no longer detected
-                        // set to 10 seconds for debugging. Normal 2-minute delay, set to 120
+                              // set to 10 seconds for debugging. Normal 2-minute delay, set to 120
 int howLong = 0;	      // this is the count of seconds (loops) so far for relay on, sound detect off
-bool isSoundDetected = false;	// will be set true when sound is detected
-bool relayOn = false;	  // will be set high when relay is active
+bool isSoundDetected = false; // will be set true when sound is detected
+bool relayOn = false;         // will be set high when relay is active
 
 void setup()
 {
   Serial.begin(9600);            // setup serial monitor
 
-  pinMode(relaypin, OUTPUT);	   // output to relay module
   pinMode(sensepin, INPUT);      // set the sensepin to read
-
+  pinMode(relaypin, OUTPUT);     // output to relay module
   digitalWrite(relaypin, HIGH);  // HIGH turns off the relay when we start
 }
 
@@ -35,7 +33,7 @@ void loop()
   }
   isSoundDetected = (directSense > 5);  // If the sum of all analog samples is over 5, then sound is detected
 
-  // these serial monitor outputs are for debugging, but they could be left in the sketch  
+  // these serial monitor outputs are useful for debugging
   Serial.print("isSoundDetected ");
   Serial.print(isSoundDetected);
   Serial.print("      relayOn ");
@@ -46,13 +44,14 @@ void loop()
   Serial.print(directSense);
   Serial.println();
   
+  // four cases: sound IS or IS NOT detected; relay is ON or OFF. If they are both same state, we only reset the counter.
   if (isSoundDetected == true)
     {
-    howLong = 0;				              // reset counter, wherever it was before
-    if (relayOn == false)		      	  // could be "else"
+    howLong = 0;                    // reset counter, wherever it was before
+    if (relayOn == false)
       {
-      digitalWrite(relaypin, LOW);	  // LOW turns on the relay
-      relayOn = true;		            	// this sets the relay active flag
+      digitalWrite(relaypin, LOW);  // LOW turns on the relay
+      relayOn = true;               // this sets the relay active flag
       }
     }
   
@@ -60,14 +59,14 @@ void loop()
     {
     if (relayOn == true)
       {
-      howLong = (howLong + 1);		    // increment the counter
-      if (howLong >= stayOn)	    	  // has delay period elapsed?
+      howLong = (howLong + 1);      // increment the counter
+      if (howLong >= stayOn)        // has delay period elapsed?
         {
-        digitalWrite(relaypin, HIGH);	// HIGH turns off the relay
-        relayOn = false;		        	// reset flag
-        howLong = 0;		            	// reset counter
+        digitalWrite(relaypin, HIGH); // HIGH turns off the relay
+        relayOn = false;            // reset flag
+        howLong = 0;                // reset counter
         }
       }
     }
-  delay(1000);				                // wait 1000 miliseconds and loop again
+  delay(1000);                      // wait 1000 miliseconds and loop again
 }
